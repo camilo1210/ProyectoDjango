6926900fcc571ecd7d2265d3c4841e05e6faacf2
Andrick Buitrago <133172342+sterraquio@users.noreply.github.com>
2024-10-30 13:04:15 -0500

    qe?

--- login/models.py
+++ login/models.py
@@ -5,3 +5,11 @@ from django.db import models
 #creo una tabla en la base de datos llamada Project
 class Project(models.Model):
     name = models.CharField(max_length=20)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    contraseña = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
--- login/views.py
+++ login/views.py
@@ -13,4 +13,25 @@ from django.contrib.auth import logout
 # Create your views here.
 
 def loginRegister(request):
+
     return render (request, 'login/login.html') #Dirige a la plantilla login.html
+    if request.method == 'POST':
+        if 'sign-in' in request.POST:  # Manejo de inicio de sesión
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirigir a la página de inicio después de iniciar sesión
+            else:
+                messages.error(request, "Credenciales incorrectas.")
+        
+        elif 'sign-up' in request.POST:  # Manejo de registro
+            name = request.POST['name']
+            email = request.POST['email']
+            password = request.POST['password']
+            user = User.objects.create_user(username=email, password=password, first_name=name)
+            user.save()
+            messages.success(request, "Registro exitoso. Ahora puedes iniciar sesión.")
+            return redirect('login')  # Redirigir al login después del registro
+
