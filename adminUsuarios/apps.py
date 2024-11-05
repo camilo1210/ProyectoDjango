3d402512270054c2bd50081793c24ee547b424fe
Andrick Buitrago <133172342+sterraquio@users.noreply.github.com>
2024-11-05 15:07:28 -0500

    Solo agregue las apps para crear la vista

---
+++ adminUsuarios/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminusuariosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adminUsuarios'
---
+++ registrarMateria/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RegistrarmateriaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'registrarMateria'
