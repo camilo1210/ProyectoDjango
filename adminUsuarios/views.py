c5af841dada7050cbaf8d3481285ccee1c443bfb
Camilo Pavas <camilopavasrios@gmail.com>
2024-11-16 18:27:03 -0500

    cambios en el HTML admin_user.html

--- adminUsuarios/views.py
+++ adminUsuarios/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import logout
+from .models import Usuario
 
 # Create your views here.
+
+from django.shortcuts import render
+from .models import Usuario  # Importa el modelo Usuario
+
+def admin(request):
+    
+    if request.method == 'POST':
+        if request.POST.get('action') == 'sign-in':  # Manejo de inicio de sesión
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirigir a la página de inicio después de iniciar sesión
+            else:
+                messages.error(request, "Credenciales incorrectas.")
+    
