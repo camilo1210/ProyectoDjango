cd29c68f0d34eb4e09786118599c1d32daec947b
Andrick Buitrago <sterraquio@hotmail.com>
2024-11-19 17:48:13 -0500

    Funciona LoginRegister e interfaz admin
    único problema es que se ve extraño la interfaz Login Register

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Materiaprima
+
 
 # Register your models here.
+
+class MateriaPrimaAdmin(admin.ModelAdmin):
+    list_display = ('idMateriaPrima', 'nombreMateriaPrima', 'costo', 'cantidad', 'unidadMedida', 'categoria', 'marca', 'fechaLlegada', 'fechaVencimiento')
+    search_fields = ('nombreMateriaPrima', 'categoria', 'marca')
+    list_filter = ('categoria', 'marca', 'unidadMedida')
+
+admin.site.register(Materiaprima)
--- login/views.py
+++ login/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth import logout
-from .models import Usuario
+from .models import Usuarios
 
 
 
@@ -17,6 +17,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .models import LoginUsuario
+from django.contrib.auth.models import User 
+from .models import Usuarios
 
 def loginRegister(request):
     if request.method == 'POST':
@@ -26,22 +28,23 @@ def loginRegister(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')  # Redirigir a la página de inicio después de iniciar sesión
+                return redirect('/admin/')  # Redirigir al admin después de iniciar sesión
             else:
                 messages.error(request, "Credenciales incorrectas.")
         
         elif request.POST.get('action') == 'sign-up':  # Manejo de registro
             name = request.POST['name']
             email = request.POST['email']
-            rol = request.POST.get('rol', 'Trabajador')
             password = request.POST['password']
-            if not LoginUsuario.objects.filter(email=email).exists():  # Evita duplicados
-                user = LoginUsuario(usuario=name, email=email, rol = rol,contraseña=password)
+            if not User.objects.filter(username=name).exists():  # Evita duplicados
+                user = User.objects.create_user(username=name, email=email, password=password)
+                user.is_staff = True
+                user.is_superuser = True  # Otorga permisos completos
                 user.save()
                 messages.success(request, "Registro exitoso. Ahora puedes iniciar sesión.")
                 return redirect('login')  # Redirigir al login después del registro
             else:
-                messages.error(request, "Este correo ya está registrado.")
+                messages.error(request, "Este nombre de usuario ya está registrado.")
     return render(request, 'login/login.html')
 '''
 def register(request):
