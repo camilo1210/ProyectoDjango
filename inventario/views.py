1ed3ec845c7dfd9ebb0cdace8d7135339e0b03d0
ninit <camilofernandezuno>
2025-04-23 21:33:44 -0500

    reporte 2 bugs

--- inventario/views.py
+++ inventario/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def listado_inventario(request):
     return render(request, 'inventario/inventario.html')
+
 def configuracion(request):
     return render(request, 'configuracion/configuraciones.html')
 
